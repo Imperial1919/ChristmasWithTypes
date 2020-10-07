4523171eb65ea9e6f7065a63d07f7a0b87593c94
Bryce <ImperialEagle1998@gmail.com>
2020-10-06 19:30:42 -0500

    New Night Class Verison.

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -3,12 +3,13 @@ namespace ChristmasWithTypes
 {
     public class Christmas
     {
-        public string[] Presents;
-        public string Santa { get; set; }
-        public int? TreeHeight { get; set; }  //TODO Make the Height property nullable
+        public string[] Presents; // List, a reference type
+        public string Santa { get; set; } // string, reference type
+        public int? TreeHeight { get; set; }  //TODO Make the Height property nullable 
+        // is of type int, a value type.
 
         //TODO Make the property, "Day", type enum
         public enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
-
+        // A enumerated list of days of the week, under the 'Day', type enumerated list.
     }
 }
--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -14,7 +14,7 @@ namespace ChristmasWithTypes
             xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{"Candy", "brandy", "1959 Buick Electra" }; 
+            xmas.Presents =  new string[3]{"Candy", "Gaming Mic", "1959 Buick Electra" }; 
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = 10;
